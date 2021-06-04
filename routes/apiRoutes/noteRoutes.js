06561d721f6b9a2add813447065eb66361c5db68
Arnaut <lou.arnaut@gmail.com>
2021-06-03 18:35:10 -0700

    Added functionality to validate note and save into file

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,4 +1,4 @@
-const { filterByQuery } = require('../../lib/notes');
+const { filterByQuery, validateNote, createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 const router = require('express').Router();
@@ -6,11 +6,22 @@ const router = require('express').Router();
 // Gets all notes - no filter
 router.get('/notes', (req, res) => {
     let results = notes;
-    console.log(notes);
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+// Saves note
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    console.log(req.body);
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is missing a title');
+    } else {
+        let note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 // Get note by ID
